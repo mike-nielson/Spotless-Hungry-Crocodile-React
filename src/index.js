e75e3893a563798abde5e900b66e10cb2911e616
mike-nielson <168361091+mike-nielson@users.noreply.github.com>
2024-04-28 13:44:59 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,15 +9,12 @@ import {
 
 import './style.css'
 import Home from './views/home'
-import NotFound1, { NotFound } from './views/not-found'
 
 const App = () => {
   return (
     <Router>
       <Switch>
         <Route component={Home} exact path="/" />
-        <Route component={NotFound1} exact path="/not-found" />
-        <Route component={NotFound} path="**" />
         <Redirect to="**" />
       </Switch>
     </Router>
